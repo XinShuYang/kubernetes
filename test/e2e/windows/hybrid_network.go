bdd62638ab4ae122c63432cd1c58655aab42d819
Shuyang Xin <gavinx@vmware.com>
2024-10-09 18:38:51 +0800

    Update google port for CI test.
    Signed-off-by: Shuyang Xin <gavinx@vmware.com>

--- test/e2e/windows/hybrid_network.go
+++ test/e2e/windows/hybrid_network.go
@@ -79,8 +79,8 @@ var _ = sigDescribe("Hybrid cluster network", skipUnlessWindows(func() {
 
 			ginkgo.By("verifying pod external connectivity to the internet")
 
-			ginkgo.By("checking connectivity to 8.8.8.8 53 (google.com) from Linux")
-			assertConsistentConnectivity(ctx, f, linuxPod.ObjectMeta.Name, linuxOS, linuxCheck("8.8.8.8", 53), externalMaxTries)
+			ginkgo.By("checking connectivity to 8.8.8.8 853 (google.com) from Linux")
+			assertConsistentConnectivity(ctx, f, linuxPod.ObjectMeta.Name, linuxOS, linuxCheck("8.8.8.8", 853), externalMaxTries)
 		})
 
 		f.It("should provide Internet connection and DNS for Windows containers", feature.NetworkingIPv4, feature.NetworkingDNS, func(ctx context.Context) {
@@ -90,7 +90,7 @@ var _ = sigDescribe("Hybrid cluster network", skipUnlessWindows(func() {
 
 			ginkgo.By("verifying pod external connectivity to the internet")
 
-			ginkgo.By("checking connectivity to 8.8.8.8 53 (google.com) from Windows")
+			ginkgo.By("checking connectivity to 8.8.8.8 853 (google.com) from Windows")
 			assertConsistentConnectivity(ctx, f, windowsPod.ObjectMeta.Name, windowsOS, windowsCheck("www.google.com"), externalMaxTries)
 		})
 
